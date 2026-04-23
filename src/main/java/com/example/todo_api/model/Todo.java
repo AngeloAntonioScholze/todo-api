21090d9867612afc88ca8baaf93322972abcb6b7
AngeloScholze <antonioscholze@gmail.com>
2026-04-23 13:08:04 -0300

    chore: created data model for todo

---
+++ src/main/java/com/example/todo_api/model/Todo.java
@@ -0,0 +1,35 @@
+package com.example.todo_api.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String title;
+    @Column
+    private String description;
+    @Column
+    private boolean  completed = false;
+    @Column(nullable=false, updatable=false)
+    @CreationTimestamp
+    private  LocalDateTime createdAt;
+    @Column(nullable=false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
